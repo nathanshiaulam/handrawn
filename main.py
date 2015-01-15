07a309ad3f544d56fbd50d7e994c011ef32aecc9
nathanshiaulam <nlam@princeton.edu>
2015-01-15 16:05:31 -0500

    added cv shit

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-import cv2                              
+import cv2                         
 import numpy as np  
+
+cap = cv2.VideoCapture(0)                #creating camera object
+
+while(cap.isOpened()):
+	ret, img = cap.read()                       
+	cv2.imshow('input',img)                 
+	k = cv2.waitKey(10)
+	if k == 27:
+		break; 
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	blur = cv2.GaussianBlur(gray,(5,5),0)
+	ret, thresh1 = cv2.threshold(blur, 70, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
