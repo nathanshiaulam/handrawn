e6827cb7f8e15d8b84488b26d0aebffca15e2036
anniechu <anyuanc@princeton.edu>
2015-01-18 18:08:46 -0500

    yeay

--- handdrawn.py
+++ handdrawn.py
@@ -1,134 +1,124 @@
 import numpy as np
 import cv2
-from flask import Flask
 
-app = Flask(__name__)
+cap = cv2.VideoCapture(0)
 
-@app.route("/")
-def showVideo():
-    points = []
-    point_set = []
-    erase_set = []
+# take first frame of the video
+ret,frame = cap.read()
 
-    draw = 0 
-    erase = 0
+drawing = np.zeros((len(frame), len(frame[0]), 3))
+#drawing = frame
+state = 0 #0 do nothing, 1 draw, 2 erase
+prevpoint = [];
+currpoint = [];
 
-    cap = cv2.VideoCapture(0)
+# setup initial location of window
+r,h,c,w = 200,40,500,40  # simply hardcoded the values
+track_window = (c,r,w,h)
 
-    # take first frame of the video
-    ret,frame = cap.read()
+# set up the ROI for trackings
+while(1):
+    ret ,frame = cap.read()
+    frame = cv2.flip(frame,1)
+    roi2 = frame[r:r+h, c:c+w]
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    roi = hsv[r:r+h, c:c+w]
 
-    # setup initial location of window
-    r,h,c,w = 200,40,500,40  # simply hardcoded the values
-    track_window = (c,r,w,h)
+    cv2.rectangle(frame, (c,r), (c+w, r+h), (255,0,0))
+    cv2.imshow('Put your color here!',frame)
+    k = cv2.waitKey(10)
+    if k ==ord('d'):
+        cv2.destroyWindow('Put your color here!')
+        break;
 
-    # set up the ROI for trackings
-    while(1):
-        ret ,frame = cap.read()
-        frame = cv2.flip(frame,1)
+rgb = [0,0,0]
+counter = 0
+
+for row in roi2:
+    for col in row:
+        rgb[0] = rgb[0] + col[0]
+        rgb[1] = rgb[1] + col[1]
+        rgb[2] = rgb[2] + col[2]
+        counter = counter + 1;
+
+rgb[0] = rgb[0]/counter;
+rgb[1] = rgb[1]/counter
+rgb[2] = rgb[2]/counter
+
+red = 0
+green = 0
+blue = 0
+counter = 0
+
+for row in roi:
+    for col in row:
+        red = red + col[0]
+        green = green + col[1]
+        blue = blue + col[2]
+        counter = counter + 1;
+
+red = red/counter;
+green = green/counter
+blue = blue/counter
+
+MIN = np.array([red - 30,green - 30,blue - 30],np.uint8)
+MAX = np.array([red + 30,green + 30,blue + 30],np.uint8)
+
+while(1):
+    ret ,frame = cap.read()
+    frame = cv2.flip(frame,1)
+    if ret == True:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        roi = hsv[r:r+h, c:c+w]
-
-        cv2.rectangle(frame, (c,r), (c+w, r+h), (255,0,0))
-        cv2.imshow('Put your color here!',frame)
-        k = cv2.waitKey(10)
-        if k ==ord('d'):
-            cv2.destroyWindow('Put your color here!')
-            break;
-
-    red = 0
-    green = 0
-    blue = 0
-    counter = 0
-
-    for row in roi:
-        for col in row:
-            red = red + col[0]
-            green = green + col[1]
-            blue = blue + col[2]
-            counter = counter + 1;
-
-    red = red/counter;
-    green = green/counter
-    blue = blue/counter
-
-    MIN = np.array([red - 30,green - 30,blue - 30],np.uint8)
-    MAX = np.array([red + 30,green + 30,blue + 30],np.uint8)
-
-    while(1):
-        ret ,frame = cap.read()
-        frame = cv2.flip(frame,1)
-        if ret == True:
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            filtered = cv2.inRange(hsv,MIN,MAX)
-            cv2.imshow('Filtered!',filtered)
-           
-            moments = cv2.moments(filtered)
-            if moments['m00']!=0:
-                cx = int(moments['m10']/moments['m00']) # cx = M10/M00
-                cy = int(moments['m01']/moments['m00'])
-
-        drawing = frame;
-        start = 0;
-        if draw == 1:
-            point_set.append((int(cx),int(cy)))
+        filtered = cv2.inRange(hsv,MIN,MAX)
+       
+        moments = cv2.moments(filtered)
+        if moments['m00']!=0:
+            cx = int(moments['m10']/moments['m00']) # cx = M10/M00
+            cy = int(moments['m01']/moments['m00'])
         else:
-            point_set = []
+            cx = -1
+            cy = -1
 
-        # if erase == 1:
-        #     erase_set.append((int(cx),int(cy)))
-        #     print "erase set" + str((int(cx),int(cy)))
-        # else:
-        #     erase_set = []
-
-
-        for idx, center in enumerate(point_set):
-            # if center not in erase_set: 
-                if center == point_set[0]:
-                    cv2.circle(drawing, center, 1, (red,green,blue), 2)
-                    marker = 0
-                else:
-                    other_center = point_set[idx-1]
-                    cv2.line(drawing, center, other_center, [red,green,blue], thickness=2,  lineType=8)
-
-
-        for set_of_points in points:
-            for idx, center in enumerate(set_of_points):
-                # if center not in erase_set:
-                    if center == set_of_points[0]:
-                        cv2.circle(drawing, center, 1, (red,green,blue), 2)
-                        marker = 0
-                    else:
-                        other_center = set_of_points[idx-1]
-                        cv2.line(drawing, center, other_center, [red,green,blue], thickness=2,  lineType=8)
-                # else:
-                #     set_of_points.remove(center)
-        
-
-        cv2.imshow('Start drawing!',drawing)
-
-        k = cv2.waitKey(1)
-        if k == ord('d'):
-            if draw == 0:
-                draw = 1
-                erase = 0
+    if cx is not -1:
+        currpoint = (int(cx),int(cy))
+        if state == 1:
+            if (prevpoint == []):
+                cv2.circle(drawing, currpoint, 1, rgb, 2)
             else:
-                draw = 0
-                points.append(point_set)
-        elif k == ord('r'):
-            points = []
-        # elif k == ord('e'):
-        #     if erase == 0:
-        #         erase = 1
-        #         draw = 0
-        #         print erase
-        #     else:
-        #         erase = 0
-        elif k == 27:
-            break
+                cv2.line(drawing, prevpoint, currpoint, rgb, thickness=2,  lineType=8)
+            prevpoint = currpoint
+        elif state == 2:
+            if (prevpoint == []):
+                cv2.circle(drawing, currpoint, 1, (0,0,0), 2)
+            else:
+                cv2.line(drawing, prevpoint, currpoint, [0,0,0], thickness=10,  lineType=8)
+            prevpoint = currpoint
 
-    cv2.destroyAllWindows()
-    cap.release()
+    #add marker
+    drawing2 = drawing.copy()
+    cv2.line(drawing2, (int(cx - 5), int(cy)), (int(cx + 5), int(cy)), [0,0,255], thickness=1,  lineType=8)
+    cv2.line(drawing2, (int(cx), int(cy-5)), (int(cx), int(cy+5)), [0,0,255], thickness=1,  lineType=8)
+    cv2.imshow('Start drawing!',drawing2)
 
-if __name__ == "__main__":
-    app.run()
+    k = cv2.waitKey(1)
+    if k == ord('d'):
+        if state == 0:
+            state = 1
+        else:
+            state = 0
+        prevpoint = []
+    elif k == ord('r'):
+        drawing = np.zeros((len(frame), len(frame[0]), 3))
+        #drawing = frame  
+        prevpoint = []
+    elif k == ord('e'):
+        if state == 2:
+            state = 0
+        else:
+            state = 2
+        prevpoint = []
+    elif k == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
