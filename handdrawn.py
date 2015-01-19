ae7de732cd9f4a7b24b4f248e083a8d73ed4b747
nathanshiaulam <nlam@princeton.edu>
2015-01-19 11:27:06 -0500

    buildpack modified

--- handdrawn.py
+++ handdrawn.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index(name=None):
      return render_template('index.html', name=name)
-
+ 
 @app.route('/showVideo')
 def showVideo():
     cap = cv2.VideoCapture(0)
